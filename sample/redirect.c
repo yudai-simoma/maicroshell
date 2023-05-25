9e4707586382651f862e9eb37d0f5d78c021bd36
yuda-simoma <cdcdasdll@icloud.com>
2023-05-25 21:35:54 +0900

    redirect(20%)

---
+++ sample/redirect.c
@@ -0,0 +1,39 @@
+#include <fcntl.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdio.h>
+
+void ft_file_write(char *path, char *str)
+{
+	int	fd;
+	int cpfd;
+
+	fd = open(path, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	cpfd = dup(STDOUT_FILENO);
+	dup2(fd, STDOUT_FILENO);
+	printf("%s", str);
+	fflush(stdout);  // stdoutのバッファをフラッシュ
+	dup2(cpfd, STDOUT_FILENO);
+	close(fd);
+}
+
+void ft_file_over_write(char *path, char *str)
+{
+	int	fd;
+	int	cpfd;
+
+	fd = open(path, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	cpfd = dup(STDOUT_FILENO);
+	dup2(fd, STDOUT_FILENO);
+	printf("%s", str);
+	fflush(stdout);  // stdoutのバッファをフラッシュ
+	dup2(cpfd, STDOUT_FILENO);
+	close(fd);
+}
+
+int main()
+{
+
+	ft_file_write("./test.txt", "ccc\n");
+	ft_file_over_write("./test2.txt", "eee\n");
+}
